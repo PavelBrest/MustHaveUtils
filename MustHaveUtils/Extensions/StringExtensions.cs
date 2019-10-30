5eab31e0a626a5c5dc32bd25401400f8c3b71dee
PavelBrest <Pasha.brest.work@gmail.com>
2019-10-30 15:39:59 +0300

    Added Rotate method and PrevPermutation for strings

--- MustHaveUtils/Extensions/StringExtensions.cs
+++ MustHaveUtils/Extensions/StringExtensions.cs
@@ -1,9 +1,72 @@
-﻿using System;
+﻿using MustHaveUtils.Helpers;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace MustHaveUtils.Extensions
 {
     public static class StringExtensions
     {
+        public unsafe static void Rotate(this string str, int from)
+        {
+            fixed (char* ptr = str)
+            {
+                char* first = &ptr[0];
+                char* f = &ptr[from] + 1;
+                char* last = &ptr[str.Length - 1] + 1;
+                Rotate(ref first, ref f, ref last);
+            }
+        }
+
+        public unsafe static bool PrevPermutation(this string str, int start, int end)
+        {
+            if (start > str.Length || end < start)
+                throw new InvalidOperationException();
+
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            fixed (char* ptr = str)
+            {
+                char* first = &ptr[start];
+                char* last = &ptr[end];
+
+                if (first == last) return false;
+
+                char* i = last;
+
+                while (true)
+                {
+                    char* i1, i2;
+
+                    i1 = i;
+                    if (*i1 < *--i)
+                    {
+                        i2 = last + 1;
+                        while (!(*--i2 < *i))
+                            ;
+                        PtrHelper.SwapIter(ref i, ref i2);
+                        PtrHelper.ReverseIter(i1, last);
+                        return true;
+                    }
+                    if (i == first)
+                    {
+                        PtrHelper.ReverseIter(first, last);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public static bool PrevPermutation(this string str, int start)
+        {
+            return PrevPermutation(str, start, str.Length - 1);
+        }
+
+        public static bool PrevPermutation(this string str)
+        {
+            return PrevPermutation(str, 0, str.Length - 1);
+        }
+
         public unsafe static bool NextPermutation(this string str, int start, int end)
         {
             if (start > str.Length || end < start)
@@ -32,14 +95,14 @@ namespace MustHaveUtils.Extensions
                         while (!(*i < *--i2))
                             ;
 
-                        SwapIter(ref i, ref i2);
-                        ReverseIter(i1, last);
+                        PtrHelper.SwapIter(ref i, ref i2);
+                        PtrHelper.ReverseIter(i1, last);
 
                         return true;
                     }
                     if (i == first)
                     {
-                        ReverseIter(first, last);
+                        PtrHelper.ReverseIter(first, last);
 
                         return false;
                     }
@@ -47,34 +110,38 @@ namespace MustHaveUtils.Extensions
             }
         }
 
-        public unsafe static bool NextPermutation(this string str, int start)
+        public static bool NextPermutation(this string str, int start)
         {
             return NextPermutation(str, start, str.Length - 1);
         }
 
-        public unsafe static bool NextPermutation(this string str)
+        public static bool NextPermutation(this string str)
         {
             return NextPermutation(str, 0, str.Length - 1);
         }
 
-        private static unsafe void SwapIter(ref char* first, ref char* second)
-        {
-            char buffer = *first;
-            *first = *second;
-            *second = buffer;
-        }
 
-        private static unsafe void ReverseIter(char* first, char* last)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private unsafe static char* Rotate(ref char* first, ref char* from, ref char* last)
         {
-            long idx = (last - first + 1) / 2;
+            if (first == from) return last;
+            if (from == last) return first;
 
-            while (idx != 0)
+            char* read = from;
+            char* write = first;
+            char* nextRead = first;
+
+            while (read != last)
             {
-                SwapIter(ref first, ref last);
-                first++;
-                last--;
-                idx--;
+                if (write == nextRead) nextRead = read;
+
+                PtrHelper.SwapIter(ref write, ref read);
+                ++write;
+                ++read;
             }
+
+            Rotate(ref write, ref nextRead, ref last);
+            return write;
         }
     }
 }
---
+++ MustHaveUtils/Helpers/PtrHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MustHaveUtils.Helpers
+{
+    internal unsafe static class PtrHelper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SwapIter(ref char* first, ref char* second)
+        {
+            char buffer = *first;
+            *first = *second;
+            *second = buffer;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ReverseIter(char* first, char* last)
+        {
+            long idx = (last - first + 1) / 2;
+
+            while (idx != 0)
+            {
+                SwapIter(ref first, ref last);
+                first++;
+                last--;
+                idx--;
+            }
+        }
+    }
+}
