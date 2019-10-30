9841eae0f864c628be3a5b404e1b5bfa866deecd
PavelBrest <Pasha.brest.work@gmail.com>
2019-10-30 19:11:31 +0300

    Added new TaskScheduler

---
+++ MustHaveUtils/Tasks/LimitedConcurrencyLevelTaskScheduler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MustHaveUtils.Tasks
+{
+    public class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
+    {
+        [ThreadStatic]
+        private static bool _currentThreadIsProcessingItems;
+
+        private readonly LinkedList<Task> _tasks = new LinkedList<Task>();
+        private readonly int _maxDegreeOfParallelism;
+
+        private int _delegatesQueuedOrRunning = 0;
+
+        public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+        {
+            if (maxDegreeOfParallelism < 1) 
+                throw new ArgumentOutOfRangeException(nameof(maxDegreeOfParallelism));
+
+            _maxDegreeOfParallelism = maxDegreeOfParallelism;
+        }
+
+        public sealed override int MaximumConcurrencyLevel { get { return _maxDegreeOfParallelism; } }
+
+        protected sealed override IEnumerable<Task> GetScheduledTasks()
+        {
+            bool lockTaken = false;
+            try
+            {
+                Monitor.TryEnter(_tasks, ref lockTaken);
+
+                if (lockTaken)
+                    return _tasks;
+
+                throw new NotSupportedException();
+            }
+            finally
+            {
+                if (lockTaken)
+                    Monitor.Exit(_tasks);
+            }
+        }
+
+        protected sealed override void QueueTask(Task task)
+        {
+            lock(_tasks)
+            {
+                _tasks.AddLast(task);
+                if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
+                {
+                    ++_delegatesQueuedOrRunning;
+                    NotifyThreadPoolOfPendingWork();
+                }
+            }
+        }
+
+        protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            if (!_currentThreadIsProcessingItems)
+                return false;
+
+            if (taskWasPreviouslyQueued)
+            {
+                return TryDequeue(task) ?
+                    base.TryExecuteTask(task) :
+                    false;
+            }
+
+            return base.TryExecuteTask(task);
+        }
+
+        protected sealed override bool TryDequeue(Task task)
+        {
+            lock (_tasks) 
+                return _tasks.Remove(task);
+        }
+
+        private void NotifyThreadPoolOfPendingWork()
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(_ =>
+            {
+                _currentThreadIsProcessingItems = true;
+
+                try
+                {
+                    while(true)
+                    {
+                        Task item;
+                        lock (_tasks)
+                        {
+                            if (_tasks.Count == 0)
+                            {
+                                --_delegatesQueuedOrRunning;
+                                break;
+                            }
+
+                            item = _tasks.First.Value;
+                            _tasks.RemoveFirst();
+                        }
+
+                        base.TryExecuteTask(item);
+                    }
+                }
+                finally
+                { _currentThreadIsProcessingItems = false; }
+            }, null);
+        }
+    }
+}
